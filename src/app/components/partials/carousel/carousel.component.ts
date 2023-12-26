28feb1512e83bf9bcbda35a50654a7ae1bd724b2
sumayya <sumayya.kareem6@gmail.com>
2023-12-26 16:36:53 +0530

    newslide

--- src/app/components/partials/carousel/carousel.component.ts
+++ src/app/components/partials/carousel/carousel.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
-
+ slides=[
+  {img:"src/assets/images/banner2.webp"},
+  {img:"src/assets/images/banner3.webp"},
+  {img:"src/assets/images/banner2.webp"},
+  {img:"src/assets/images/banner3.webp"},
+  {img:"src/assets/images/banner2.webp"},
+  {img:"src/assets/images/banner3.webp"},
+  {img:"src/assets/images/banner2.webp"},
+  {img:"src/assets/images/banner3.webp"},
+  {img:"src/assets/images/banner2.webp"},
+  {img:"src/assets/images/banner3.webp"}
+ ];
+ slideConfig={
+  "slidesToShow":4,
+  "slidesToScroll":4,
+  "autoplay":true,
+  "autoplaySpeed":5000,
+  "pauseOnHover":true,
+  "infinite":true,
+  "responsive":[
+    {
+      "breakpoint":992,
+      "settings":{
+        "arrows":true,
+        "infinite":true,
+        "slidesToShow":3,
+        "slidesToScroll":3
+      }
+    },
+    {
+      "breakpoint":768,
+      "settings":{
+        "arrows":true,
+        "infinite":true,
+        "slidesToShow":1,
+        "slidesToScroll":1
+      }
+    }
+  ]
+ };
 }
